22107a903a48e861f7896b6e0afb5e467cb035db
Baljit Khabra <baljitkhabra99@gmail.com>
2022-06-03 18:12:07 -0700

    complete

---
+++ block-BNaaau/server5000.js
@@ -0,0 +1,13 @@
+let http = require("http");
+
+let server = http.createServer(hanldeRequest);
+
+function hanldeRequest(req, res) {
+  console.log("Request", req);
+  console.log("Response", res);
+  res.end("Welcome");
+}
+
+server.listen(5000, () => {
+  console.log("server listening on port 5k");
+});
